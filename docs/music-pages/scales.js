2eb5d43fe90c9d6f0bc85c6acf80de61d5154cb5
GLEN CUNNINGHAM <glen@cunninghamconsulting.net>
2024-04-03 17:48:44 -0400

    Package updates.  Add enclosure example.

--- docs/music-pages/scales.js
+++ docs/music-pages/scales.js
@@ -14,3 +14,10 @@ K:D
 DEFG ABcd|edcB AGFE|D4z4|
 `;
 
+export const c_maj_encl = `
+X:1
+T:C Major with Diatonic/Chromatic 2 note enclosures
+K:C
+(3DB,C (3E^CD (3F^DE (3GEF | (3A^FG (3B^GA (3c^AB (3dBc |
+(3dBc (3c^AB (3B^G=A (3A^F=G | (3GEF (3F^DE (3E^C=D (3DB,=C ||
+`;
